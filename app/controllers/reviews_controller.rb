1eb95f989f5f1a764b42f4c17d5fd990a9650df7
Wilson Wang <wilsonw926@berkeley.edu>
2019-12-04 11:08:47 -0800

    new review form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,22 @@
 class ReviewsController < ApplicationController
 
+    def new
+        if current_user
+            set_restaurant()
+        else
+            redirect_to new_user_session_path
+        end
+    end
+
+    def create
+        set_restaurant()
+        @review = current_user.reviews.build(review_params)
+        @review.venue = @venue
+        @review.votes = 0
+  		@review.save
+        redirect_to venue_path(@venue)
+    end
+
     def upvote
         id = params[:id]
         review = Review.find(id)
@@ -16,4 +33,13 @@ class ReviewsController < ApplicationController
         redirect_to venue_path(params[:venue])
     end
 
+    private
+
+    def set_restaurant
+        @venue = Venue.find(params[:venue_id])
+    end
+
+    def review_params
+        params.require(:review).permit(:stars, :text)
+    end
 end
