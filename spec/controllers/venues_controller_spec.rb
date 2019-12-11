a422a4266e85c0a9c9410b92070ccd073931d1aa
John Yang <john.yang20@berkeley.edu>
2019-12-10 21:46:52 -0800

    Cleaned up venues controller spec implementation, replaced count references with expect-to-change clauses

--- spec/controllers/venues_controller_spec.rb
+++ spec/controllers/venues_controller_spec.rb
@@ -3,27 +3,29 @@ require 'rails_helper'
 describe VenuesController do
   describe "create" do
     it "should increase the total number of venues by 1" do
-      difference = 1
-      initial_count = Venue.count
-      Venue.create(:venue_name => 'Wheeler Hall', :location => 'Berkeley', season: "Season (Y/N/S)")
-      final_count = Venue.count
-      expect(final_count - initial_count).to eq(difference)
+      expect { post :create,
+        venue: {
+          :venue_name => 'Wheeler Hall',
+          :location => 'Berkeley',
+          season: "Season (Y/N/S)"
+        }
+      }.to change(Venue, :count).by(1)
     end
   end
 
   describe "destroy" do
     it "should decrease the total number of venues by 1" do
-      difference = 1
-      initial_count = Venue.count
-      Venue.create(:venue_name => 'Wheeler Hall', :location => 'Berkeley', season: "Season (Y/N/S)")
-      final_count = Venue.count
-      expect(final_count - initial_count).to eq(difference)
+      expect { post :create,
+        venue: {
+          :venue_name => 'Wheeler Hall',
+          :location => 'Berkeley',
+          season: "Season (Y/N/S)"
+        }
+      }.to change(Venue, :count).by(1)
 
-      difference = -1
-      initial_count = Venue.count
-      Venue.destroy(1)
-      final_count = Venue.count
-      expect(final_count - initial_count).to eq(difference)
+      expect { post :destroy,
+        :id => 1
+      }.to change(Venue, :count).by(-1)
     end
   end
 end
