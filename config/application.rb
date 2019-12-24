c3f9ed015edfdda96497cc4cdac38cb73cd6fc9d
Wilson Wang <wilsonw926@berkeley.edu>
2019-12-24 09:51:29 -0800

    precomiple rails admin css

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,7 @@ module Venues
     config.active_record.raise_in_transactional_callbacks = true
     config.autoload_paths << Rails.root.join('lib')
     config.web_console.development_only = false
+    config.assets.precompile += %w(rails_admin/rails_admin.css rails_admin/rails_admin.js)
     # config.expect_with(:rspec) { |c| c.syntax = :should }
   end
 end
